bf667b09144e206f37faf01e8aac97897abae429
unknown <edudakovic@student.unipu.hr>
2022-08-19 19:17:25 +0200

    napravljen change password u settingse

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,7 +1,45 @@
-<template></template>
+<template>
+  <div>
+    <div class="container razmak pozadina">
+      <button
+        type="button"
+        @click="passwordReset()"
+        class="btn mybutton razmacic"
+      >
+        Reset password
+      </button>
+    </div>
+  </div>
+</template>
 
 <script>
+import { firebase } from "@/firebase";
+import store from "@/store.js";
+
 export default {
   name: "Settings",
+
+  methods: {
+    passwordReset() {
+      firebase
+        .auth()
+        .sendPasswordResetEmail(store.User)
+        .then(() => {
+          // Password reset email sent!
+          // ..
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ..
+        });
+    },
+  },
 };
 </script>
+
+<style>
+.razmacic {
+  margin: 50px;
+}
+</style>
