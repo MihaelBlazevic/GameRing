9a3a4e12e52d68b9dd0a7e7c050d51d25adb56ea
unknown <mihael75.blazevic@gmail.com>
2022-08-24 14:31:30 +0200

    Popravljen news

--- src/views/News.vue
+++ src/views/News.vue
@@ -2,7 +2,7 @@
   <iframe
     width="85%"
     height="1600"
-    src="https://rss.app/embed/v1/wall/bKQi9Ni1pUhcD5Sf"
+    src="https://rss.app/embed/v1/wall/tzVX9MXe6vmQ8b7k"
     frameborder="0"
   ></iframe>
 </template>
