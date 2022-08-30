729e96343d3586f48bb843b62eabf4aca1c2e66c
unknown <mihael75.blazevic@gmail.com>
2022-08-30 22:27:01 +0200

    popravljen rout dok se log out na Home

--- src/App.vue
+++ src/App.vue
@@ -100,7 +100,7 @@ export default {
         .auth()
         .signOut()
         .then(() => {
-          this.$router.push({ name: "Login" });
+          this.$router.push({ name: "Home" });
         });
     },
   },
